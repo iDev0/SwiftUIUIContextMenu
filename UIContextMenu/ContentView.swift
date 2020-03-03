b4009d72d3811af3e4106856edaa26647398962a
SamplesMaker <insert@kakao.com>
2020-03-03 12:36:04 +0900

    Initial Commit

---
+++ UIContextMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIContextMenu
+//
+//  Created by iDev0 on 2020/03/03.
+//  Copyright © 2020 Ju Young Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
